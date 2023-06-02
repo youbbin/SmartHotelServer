c3be252478bb5888890cf0beac6c38ee94650a52
youbin <youbin5478@naver.com>
2023-06-02 11:50:39 +0900

    api 수정

--- src/main/java/com/example/smarthotelserver/dto/AirQualityRequestDto.java
+++ src/main/java/com/example/smarthotelserver/dto/AirQualityRequestDto.java
@@ -3,7 +3,7 @@ package com.example.smarthotelserver.dto;
 import lombok.Getter;
 
 @Getter
-public class AirPurifierDto {
+public class AirQualityDto {
     private Long roomNumber; // 객실번호(PK)
     private double gasConcentration; // 가스 농도
     private double temperature; // 온도
